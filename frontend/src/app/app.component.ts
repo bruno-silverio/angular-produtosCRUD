d631d1b4ce541df7cfe20a5dc9e68a7b9e05e2ce
Bruno Silverio <bcamilo94@gmail.com>
2024-03-08 11:13:43 -0300

    feat: add header component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/template/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  //imports: [RouterOutlet],
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    standalone: true,
+    //imports: [RouterOutlet],
+    templateUrl: 'app.component.html',
+    imports: [HeaderComponent]
 })
 export class AppComponent {
   title = 'frontend';
