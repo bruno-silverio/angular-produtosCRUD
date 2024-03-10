af3caaa4da51139dea69685bcf0aaa9e3ab338d7
Bruno Silverio <bcamilo94@gmail.com>
2024-03-10 20:02:14 -0300

    feat: add product service

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatButtonModule } from "@angular/material/button";
+
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
 
+  constructor(private productService: ProductService, private router: Router) { }
+
+  ngOnInit(): void { }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto criado!')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
