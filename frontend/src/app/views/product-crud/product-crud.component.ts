c734e60e189926a30e4048404f034a90b49a9019
Bruno Silverio <bcamilo94@gmail.com>
2024-03-10 19:15:23 -0300

    feat: add navigate to product-create

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
+
+import { ProductCreateComponent } from "../../components/product/product-create/product-create.component";
 
 @Component({
   selector: 'app-product-crud',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    ProductCreateComponent
+  ],
   templateUrl: './product-crud.component.html',
   styleUrl: './product-crud.component.css'
 })
-export class ProductCrudComponent {
+export class ProductCrudComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
+  }
 }
