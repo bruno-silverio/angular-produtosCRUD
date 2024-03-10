084288d169a4c87cb508d980f8232414b85ffb9b
Bruno Silverio <bcamilo94@gmail.com>
2024-03-10 19:13:52 -0300

    feat: add structural directives

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,27 @@
+import {
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
+
+@Directive({
+  selector: '[appFor]',
+  standalone: true
+})
+export class ForDirective implements OnInit {
+  @Input("myForEm") numbers: number[] = [];
+
+  constructor(
+    private container: ViewContainerRef,
+    private template: TemplateRef<any>
+  ) { }
+
+  ngOnInit(): void {
+    for (let number of this.numbers) {
+      this.container.createEmbeddedView(
+        this.template, { $implicit : number });
+    }
+  }
+}
