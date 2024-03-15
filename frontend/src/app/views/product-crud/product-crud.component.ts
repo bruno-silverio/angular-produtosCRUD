e6397d890489e8f49ab16fbb47664e02bce0942d
Bruno Silverio <bcamilo94@gmail.com>
2024-03-15 10:13:18 -0300

    feat: add get product component

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -3,16 +3,18 @@ import { Router } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
 
 import { ProductCreateComponent } from "../../components/product/product-create/product-create.component";
+import { ProductReadComponent } from "../../components/product/product-read/product-read.component";
 
 @Component({
-  selector: 'app-product-crud',
-  standalone: true,
-  imports: [
-    MatButtonModule,
-    ProductCreateComponent
-  ],
-  templateUrl: './product-crud.component.html',
-  styleUrl: './product-crud.component.css'
+    selector: 'app-product-crud',
+    standalone: true,
+    templateUrl: './product-crud.component.html',
+    styleUrl: './product-crud.component.css',
+    imports: [
+        MatButtonModule,
+        ProductCreateComponent,
+        ProductReadComponent
+    ]
 })
 export class ProductCrudComponent implements OnInit {
 
