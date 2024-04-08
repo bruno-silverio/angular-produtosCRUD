fb646853e7425b0c10e2b1b9e994bfd34bbf50cf
Bruno Silverio <bcamilo94@gmail.com>
2024-04-08 17:02:41 -0300

    feat: add products table

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor, registerLocaleData } from '@angular/common';
+import { MatTableModule } from '@angular/material/table'
+import { RouterModule } from '@angular/router';
+
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 @Component({
   selector: 'app-product-read',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, MatTableModule, RouterModule, CommonModule],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   templateUrl: './product-read.component.html',
   styleUrl: './product-read.component.css'
 })
 export class ProductReadComponent implements OnInit {
   products!: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action']
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
